b9416a3644ca840970e17237f3e78187b737fe8f
Hambalkó Bence <hambalko.bence@gmail.com>
2020-11-23 23:52:33 +0100

    Fix training with latest detectron2

--- train_net.py
+++ train_net.py
@@ -50,7 +50,7 @@ class Trainer(DefaultTrainer):
             model = DistributedDataParallel(
                 model, device_ids=[comm.get_local_rank()], broadcast_buffers=False
             )
-        super(DefaultTrainer, self).__init__(model, data_loader, optimizer)
+        super().__init__(model, data_loader, optimizer)
 
         self.scheduler = self.build_lr_scheduler(cfg, optimizer)
         # Assume no other objects need to be checkpointed.
